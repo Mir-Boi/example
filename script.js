5f07f7abac6fae601170613e17c5d749d80387c5
smir-ant <abcsmir.ant@gmail.com>
2023-07-22 14:21:27 +0700

    commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const tg = window.Telegram.WebApp
+tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
 tg.isExpanded
 tg.expand()
+
+let informazia = {
+    		score: config.countScore,
+    		text: "anime"
+    	}
+alert("Щас отправлю в тг")
+tg.sendData(JSON.stringify(informazia))  // js -> json 
+alert("Отрпавил в тг!")
