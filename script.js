dd2ce35f70a873a105e81d74503023001e26339c
smir-ant <abcsmir.ant@gmail.com>
2023-07-22 14:24:33 +0700

    alerts + fixes

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+console.log("Тг создам")
 const tg = window.Telegram.WebApp
-tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
+console.log("Тг создал")
+// tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
+alert(`Добро пожаловать, @${tg.WebAppUser.username}.`)
 tg.isExpanded
 tg.expand()
+сconsole.log("Расширил")
 
 let informazia = {
-    		score: config.countScore,
+    		score: 123,
     		text: "anime"
     	}
 alert("Щас отправлю в тг")
